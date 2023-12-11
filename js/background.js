7202f2afe33eb6ba7b0813c09a2c871f040172e6
inseis <inseo1563@naver.com>
2023-12-11 20:51:14 +0900

    weather changed

--- js/background.js
+++ js/background.js
@@ -1,20 +1,34 @@
-// background.js 파일
-const img = ["chihiro014", "chihiro043", "karigurashi002", "karigurashi028", "mononoke024", "ponyo050", "chihiro015"];
+const img = {
+  spring: "karigurashi002",
+  summer: "ponyo050",
+  autumn: "chihiro014",
+  winter: "mononoke024"
+};
 
-const chosenImage = img[Math.floor(Math.random() * img.length)];
-
-// js에서 이미지 태그 생성하기
-const bgImage = document.createElement("img");
-
-// 크리스마스 날짜 확인
 const today = new Date();
+let chosenImage = "";
+
+// 크리스마스 확인
 if (today.getMonth() === 11 && today.getDate() === 25) {
-  // 크리스마스인 경우 크리스마스 이미지 사용
-  bgImage.src = "./images/christmas_background.jpg";
+  chosenImage = "./images/christmas_background.jpg";
 } else {
-  // 그 외의 경우 랜덤 이미지 사용
-  bgImage.src = `./images/${chosenImage}.jpg`;
+  // 계절에 따른 이미지 선택
+  const month = today.getMonth();
+  if (month >= 2 && month <= 4) {
+    chosenImage = img.spring;
+  } else if (month >= 5 && month <= 7) {
+    chosenImage = img.summer;
+  } else if (month >= 8 && month <= 10) {
+    chosenImage = img.autumn;
+  } else {
+    chosenImage = img.winter;
+  }
+  chosenImage = `./images/${chosenImage}.jpg`;
 }
+
+// 이미지 태그 생성
+const bgImage = document.createElement("img");
+bgImage.src = chosenImage;
 
 // 생성한 태그 html에 넣어주기
 const body = document.body;
